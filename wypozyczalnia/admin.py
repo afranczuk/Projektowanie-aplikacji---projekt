ac75e5289f4b5ae3704b9883cd202ebda6dc815c
Aleksandra Frańczuk <aleksandrafranczuk@MacBook-Air-Aleksandra.local>
2026-01-23 19:41:17 +0100

    zmiany w models i w admin, dodanie do settings wypozyczalni

--- wypozyczalnia/admin.py
+++ wypozyczalnia/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Samochod, Kategoria
 
-# Register your models here.
+class SamochodAdmin(admin.ModelAdmin):
+    list_display = ['marka', 'model', 'kategoria', 'data_dodania']
+    list_filter = ['kategoria', 'data_dodania']
+    search_fields = ['marka', 'model']
+
+admin.site.register(Samochod, SamochodAdmin)
+admin.site.register(Kategoria)
--- wypozyczalnia/models.py
+++ wypozyczalnia/models.py
@@ -1,3 +1,20 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+class Kategoria(models.Model):
+    nazwa = models.CharField(max_length=100)
+    opis = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.nazwa
+
+class Samochod(models.Model):
+    marka = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    data_dodania = models.DateField(default=timezone.now)
+    kategoria = models.ForeignKey(Kategoria, on_delete=models.CASCADE)
+    wlasciciel = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.marka} {self.model}"
