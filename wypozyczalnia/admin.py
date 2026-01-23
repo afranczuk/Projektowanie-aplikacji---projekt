417992286d65001aaa962d3ded05e9088b78b810
Aleksandra Frańczuk <aleksandrafranczuk@MacBook-Air-Aleksandra.local>
2026-01-23 20:54:05 +0100

    Dodanie walidacji ceny, aby nie mozna bylo dodac ceny ujemnej

--- wypozyczalnia/admin.py
+++ wypozyczalnia/admin.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from .models import Samochod, Kategoria
 
 class SamochodAdmin(admin.ModelAdmin):
-    list_display = ['marka', 'model', 'kategoria', 'data_dodania']
+    list_display = ['marka', 'model', 'cena_za_dobe','kategoria', 'data_dodania']
     list_filter = ['kategoria', 'data_dodania']
     search_fields = ['marka', 'model']
 
 admin.site.register(Samochod, SamochodAdmin)
 admin.site.register(Kategoria)
+
--- wypozyczalnia/models.py
+++ wypozyczalnia/models.py
@@ -1,6 +1,6 @@
-
 from django.db import models
 from django.utils import timezone
+from django.core.validators import MinValueValidator
 
 class Kategoria(models.Model):
     nazwa = models.CharField(max_length=100)
@@ -12,20 +12,10 @@ class Kategoria(models.Model):
 class Samochod(models.Model):
     marka = models.CharField(max_length=100)
     model = models.CharField(max_length=100)
+    cena_za_dobe = models.DecimalField(max_digits=8, decimal_places=2, default=0.00, validators=[MinValueValidator(1.00)])
     data_dodania = models.DateField(default=timezone.now)
     kategoria = models.ForeignKey(Kategoria, on_delete=models.CASCADE)
     wlasciciel = models.ForeignKey('auth.User', on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.marka} {self.model}"
-    
-class Samochod(models.Model):
-    marka = models.CharField(max_length=100)
-    model = models.CharField(max_length=100)
-    cena_za_dobe = models.DecimalField(max_digits=8, decimal_places=2, default=0.00)
-    data_dodania = models.DateField(default=timezone.now)
-    kategoria = models.ForeignKey(Kategoria, on_delete=models.CASCADE)
-    wlasciciel = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f"{self.marka} {self.model} - {self.cena_za_dobe} zł" 
+        return f"{self.marka} {self.model} - {self.cena_za_dobe} zł"
